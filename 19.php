b2c74da9b8b0090fc3e3c64cdf2fcb6a284626af
sunquan110 <50441620+sunquan110@users.noreply.github.com>
2019-05-31 08:45:04 +0800

    Add files via upload

---
+++ 19.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 孙权
+ * Date: 2019/5/31
+ * Time: 8:37
+ */
+$arr= [1,2,3,4,5];
+function one($arr){
+    foreach($arr as $k=>$value){
+        $a =$value;
+    }
+}
