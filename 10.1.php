03afd34756ceea0a739a6a49322b06c1839eaa27
sunquan110 <50441620+sunquan110@users.noreply.github.com>
2019-06-19 08:48:19 +0800

    Add files via upload

---
+++ 10.1.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 孙权
+ * Date: 2019/6/19
+ * Time: 8:42
+ */
+$array = [2,4,3,6,3,2,5,5];
+function FindNumsAppearOnce($array){
+    $cut = array_count_values($array);
+    foreach($cut as $k=>$v){
+        if($v==1){
+       $arr[]=$k;
+        }
+
+    }
+    return $array;
+}
+
+print_r(FindNumsAppearOnce($array));
